61d09eb7a6789a879be7554f8fa09bc6e1fb83ec
mxdhyj1995@hotmail.com <mxdhyj1995@hotmail.com>
2018-08-12 01:40:42 -0400

    search and post result to result page

--- function/Test.py
+++ function/Test.py
@@ -1,4 +1,5 @@
-import item
+from modules import item
 
 soup = item.find_search_content("https://www.youtube.com/results?search_query=python")
+print(item.every_video(soup))
 print(item.page_bar(soup))
---
+++ run.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template,request
+from modules import item
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("base.html")
+
+@app.route("/result")
+# get input from name=searuch,
+def result_page():
+    search = request.args.get('search')
+    soup = item.find_search_content(search)
+    all_item = item.every_video(soup)
+    #take value of search and bring to result page
+    return render_template("result.html",search=search,all_item = all_item )
+
+if __name__=="__main__":
+    app.run()
